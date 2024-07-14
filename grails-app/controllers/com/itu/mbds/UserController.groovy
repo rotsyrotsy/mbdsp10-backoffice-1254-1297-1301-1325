881b301d605bb3f1fb07ec80368f8f586632a77e
rotsyrotsy <80813050+rotsyrotsy@users.noreply.github.com>
2024-07-15 00:06:35 +0300

    Create, Show user

--- grails-app/controllers/com/itu/mbds/UserController.groovy
+++ grails-app/controllers/com/itu/mbds/UserController.groovy
@@ -1,6 +1,11 @@
 package com.itu.mbds
 
 import grails.plugin.springsecurity.annotation.Secured
+import grails.validation.ValidationException
+import org.springframework.web.multipart.MultipartFile
+
+import static org.springframework.http.HttpStatus.CREATED
+import static org.springframework.http.HttpStatus.NOT_FOUND
 
 @Secured(['ROLE_ADMIN','ROLE_USER','ROLE_SUPER_ADMIN'])
 
@@ -13,8 +18,66 @@ class UserController {
         params.order = "desc"
         respond userService.list(params), model:[userCount: userService.count()]
     }
-    def show(Long id) {
-    }
     def create() {
+        respond new User(params), model:[roles:Role.list()]
+    }
+    def save(User user) {
+        if (user == null) {
+            notFound()
+            return
+        }
+        try {
+            // Getting the file from the form
+            MultipartFile f = request.getFile('image')
+            if (f != null && !f.empty) {
+                // Define the target directory and filename
+                String originalFilename = "${new Date().getTime()}_${f.originalFilename}"
+                // Get the path of illustration - path inside application.groovy
+                String mainPath = grailsApplication.config.illustrations.path
+                File targetDir = new File(mainPath)
+                // Create the directory if it does not exist
+                if (!targetDir.exists()) {
+                    targetDir.mkdirs()
+                }
+                // Define the target file
+                File targetFile = new File(targetDir, originalFilename)
+                // Transfer the file to the target location
+                f.transferTo(targetFile)
+
+                // Affect user image to the name of the file
+                user.user_image = originalFilename
+            }
+            user.role = Role.get(params.role)
+            userService.save(user)
+        } catch (ValidationException e) {
+            respond user.errors, view:'create'
+            return
+        }
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'user.label', default: 'User'), user.id])
+                redirect method:"GET", action: "show", id: user.id
+            }
+            '*' { respond method:"GET", action: "show", id: user.id, [status: CREATED] }
+        }
+    }
+    def show(Long id) {
+        def user = userService.get(id)
+        if (user == null) {
+            notFound()
+            return
+        }
+        respond user
+    }
+
+    protected void notFound() {
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'user.label', default: 'User'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*'{ render status: NOT_FOUND }
+        }
     }
 }
